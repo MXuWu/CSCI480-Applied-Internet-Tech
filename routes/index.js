659f9a68a8117ba247c24619d2bf586bd1b50bd8
Jazz <michael13816@gmail.com>
2017-11-09 01:50:17 -0500

    trying to deploy

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,37 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
+require('../db');
+
+
+const Exercise = mongoose.model('Exercise');
+
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    Exercise.find((err, exercises) =>{
+        if (err){
+            throw err;
+        } else {
+            res.render('index', { title: 'Express', exercises: exercises});
+        }
+    })
+});
+
+router.post('/', function (req, res) {
+    const exercise = new Exercise({
+        name: req.body.name,
+        reps: req.body.reps,
+        sets: req.body.sets,
+        goal: req.body.goal
+    });
+    exercise.save((err) => {
+        if (err) {
+            throw err;
+        } else {
+            res.redirect('/');
+        }
+    });
 });
 
 module.exports = router;
